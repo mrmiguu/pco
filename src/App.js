22a3ed4d6246dabf8f752fa1aa1b024b620bbf75
Alex <a.matulionis@usa.com>
2018-12-26 23:21:40 -0800

    Started to tile cube.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './styles/App.css'
+import tiles_desert from './assets/tiles-desert.png'
 
 const sides = [
   'front',
@@ -9,6 +10,29 @@ const sides = [
   'top',
   'bottom'
 ]
+
+const Tile = ({ xy: [x, y] }) => (
+  <div className="tile"
+    style={{
+      top: `${x*10}%`,
+      left: `${y*10}%`,
+      backgroundImage: `url(${tiles_desert})`
+    }}
+  ></div>
+)
+
+const Face = ({ side }) => (
+  <div className={`face ${side || sides[0]}`}>
+    {
+      [...Array(100).keys()].map(i =>
+        <Tile
+          key={i}
+          xy={[~~(i%10), ~~(i/10)]}
+        />
+      )
+    }
+  </div>
+)
 
 class App extends Component {
 
@@ -20,25 +44,29 @@ class App extends Component {
     return <>
 
       <div className="scene">
-        <div className={`cube ${side || sides[0]}`}>
-          <div className="face front">FRO</div>
-          <div className="face back">BAC</div>
-          <div className="face right">RIG</div>
-          <div className="face left">LEF</div>
-          <div className="face top">TOP</div>
-          <div className="face bottom">BOT</div>
+        <div className={`camera ${side || sides[0]}`}>
+          {
+            sides.map(side =>
+              <Face
+                key={side}
+                side={side}
+              />
+            )
+          }
         </div>
       </div>
 
       <p className="radio-group">
-        {sides.map(side => (
+        {
+          sides.map(side =>
             <input
               key={side}
               type="radio"
               name="rotate-cube-side"
               onChange={() => this.setState({ side })}
             />
-        ))}
+          )
+        }
       </p>
 
     </>
